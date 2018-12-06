d7d71f481cfd9faa2ef4960754d5e78d46a5c566
Matt Ridgway <matt.ridgway@zupa.co.uk>
2018-12-06 19:25:48 +0000

    Added MVC and empty books controller

---
+++ src/Sample.Cosmos.BookApi/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sample.Cosmos.BookApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
